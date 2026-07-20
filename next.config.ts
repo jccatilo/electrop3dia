1e35b9bd30fbfa37c715ca71ddd736a7487ab70d
Nightfall3594 <briankristoferperez28@gmail.com>
2026-07-20 08:42:34 +0800

    chore: update dev config to use local network

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@
 const nextConfig = {
   output: 'export',  // This enables static HTML export
   reactStrictMode: true,
+  allowedDevOrigins: [
+    '0.0.0.0',
+    '192.168.103.118'
+  ],
   swcMinify: true,
   images: {
     unoptimized: true, // Required for static export
